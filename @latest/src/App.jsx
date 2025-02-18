1f4df8f9f4a96a859777f67a046fec6b4e63c4f9
swandrack <swandrack@gmail.com>
2025-02-18 14:29:57 -0500

    Added environment input

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -3,18 +3,39 @@ import React from "react";
 
 export default function App() {
   const [guid, setGuid] = useState("");
+  const [env, setEnv] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    (function() {
-      var walkme = document.createElement('script'); 
-      walkme.type = 'text/javascript'; 
-      walkme.async = true; 
-      walkme.src = `https://cdn.walkme.com/users/${guid}/test/walkme_${guid}_https.js`; 
-      var s = document.getElementsByTagName('script')[0]; 
-      s.parentNode.insertBefore(walkme, s); 
-      window._walkmeConfig = {smartLoad:true}; 
-    })();
+    try {
+      (function() {
+        var walkme = document.createElement('script'); 
+        walkme.type = 'text/javascript'; 
+        walkme.async = true; 
+        walkme.src = `https://cdn.walkme.com/users/${guid}${isEnv(env)}/walkme_${guid}_https.js`; 
+        var s = document.getElementsByTagName('script')[0]; 
+        s.parentNode.insertBefore(walkme, s); 
+        window._walkmeConfig = {smartLoad:true}; 
+      })();
+    } catch (error) {
+      alert("Snippet is already loaded")
+    }
+  }
+  const removeWalkMe = (event) =>{
+    try {
+      event.preventDefault()
+      _walkMe.removeWalkMe()
+    } catch (error) {
+      alert("WalkMe is not running!")
+    }
+  }
+
+  function isEnv(env) {
+    if (env) {
+      return '/' + env
+    } else {
+      return ""
+    }
   }
 
   return(
@@ -25,7 +46,17 @@ export default function App() {
       value={guid}
       onChange={(e) => setGuid(e.target.value)}
       />
-      <input type="submit" onClick={handleSubmit}/>
     </label>
+    <br />
+    <label>Please enter Env here: /
+      <input
+      type="text"
+      value={env}
+      onChange={(e) => setEnv(e.target.value)}
+      />
+    </label>
+    <br />
+      <input type="submit" onClick={handleSubmit}/>
+      <input type="submit" className="removeWalkMeButton" onClick={removeWalkMe} />
   </form>
 )}

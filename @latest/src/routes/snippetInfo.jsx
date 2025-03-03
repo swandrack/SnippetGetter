4518dc16a313cb99ff153b40ff3c6dce7fe1bab0
Alec Lucas <>
2025-03-02 19:01:39 -0500

    doing this for you

--- @latest/src/routes/snippetInfo.jsx
+++ @latest/src/routes/snippetInfo.jsx
@@ -1,12 +1,10 @@
-import React from "react"
-
 export default function SnippetInfo() {
   try {
   return(
     <>
     <h3>Snippet Log:</h3>
-      {_walkmeInternals ? (
-        <p>{_walkmeInternals.snippetLog}</p>
+      {window._walkmeInternals ? (
+        <p>{window._walkmeInternals.snippetLog}</p>
       ) : (
         <p>???</p>        
       )

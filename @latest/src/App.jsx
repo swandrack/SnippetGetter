32970a73a20dcd125965618d91fd7081234890b5
swandrack <swandrack@gmail.com>
2025-03-09 18:14:37 -0400

    fixed shitty WalkMe on load

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -5,23 +5,21 @@ import { loadWalkMe } from "./utils/loadWalkme";
 import { useState, useEffect } from "react"
 import "./index.css";
 
-async function statusReport() {
-    if (window._walkmeInternals?.removeWalkMeReason) {
-        alert(window._walkmeInternals.removeWalkMeReason);
-    }
-}
-
 export default function App() {
     const guid = window.localStorage.getItem("guid");
     const env = window.localStorage.getItem("env");
 
-    if (guid || env != "") {
-        if (env == "production" || env == null) {
-            loadWalkMe(guid, "")
-        } else {
-            loadWalkMe(guid, `/${env}`)
+    useEffect(() => {
+    if (guid != "null") {
+        if (guid != null){
+            if (env == "production" || env == null || env == "null") {
+                loadWalkMe(guid, "")
+            } else {
+                loadWalkMe(guid, `/${env}`)
+            }
         }
     }
+}, []);
     return (
         <SnackbarProvider>
             <div style={{ display: "flex", flexDirection: "row", gap: 4 }}>
